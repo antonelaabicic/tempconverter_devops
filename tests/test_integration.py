0ad1af608d625cc623a47233c2e1792f45df5f55
Antonela Abicic <antonela.abicic@gmail.com>
2026-07-06 00:15:32 +0200

    updated unit and integration test

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,6 +1,18 @@
 from app import app, Temperature
 
-def test_temperature_saved_correctly(client):
+def test_temperature_record_is_created(client):
+    with app.app_context():
+        before = Temperature.query.count()
+
+    response = client.post("/", data={"celsius": "25"}, follow_redirects=True)
+
+    with app.app_context():
+        after = Temperature.query.count()
+
+    assert response.status_code == 200
+    assert after == before + 1
+
+def test_temperature_values_are_saved_correctly(client):
     client.post("/", data={"celsius": "30"}, follow_redirects=True)
 
     with app.app_context():
@@ -9,15 +21,3 @@ def test_temperature_saved_correctly(client):
     assert latest is not None
     assert latest.celsius == 30
     assert latest.fahrenheit == 86.0
-
-def test_temperature_saved_correctly(client):
-    with app.app_context():
-        before = Temperature.query.count()
-
-    response =  client.post("/", data={"celsius": "25"}, follow_redirects=True)
-
-    with app.app_context():
-        after = Temperature.query.count()
-
-    assert response.status_code == 200
-    assert after == before + 1
--- tests/test_unit.py
+++ tests/test_unit.py
@@ -1,11 +1,11 @@
-def test_home_page(client):
+def test_home_page_returns_200(client):
     response = client.get("/")
     assert response.status_code == 200
 
-def test_student_name(client):
+def test_student_name_is_displayed(client):
     response = client.get("/")
     assert b"Antonela Abicic" in response.data
 
-def test_college_name(client):
+def test_college_name_is_displayed(client):
     response = client.get("/")
     assert b"Algebra University College" in response.data
